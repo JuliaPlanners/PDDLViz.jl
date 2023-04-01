55a9af895bd4d8bc97509a235fa172c07bd286e7
Xuan <tanqazx@gmail.com>
2023-04-01 16:29:14 -0400

    Fix stroke visibility.

--- src/graphics/recipes.jl
+++ src/graphics/recipes.jl
@@ -20,10 +20,11 @@ function Makie.plot!(plt::GraphicPlot{<:Tuple{BasicGraphic}})
     # Plot fill
     Makie.mesh!(plt, shape; attributes...)
     # Plot stroke if width is greater than 0
-    attributes[:visible] = (get(attributes, :visible, false)[] &&
-                            get(attributes, :strokewidth, 0)[] > 0)
+    visible = get!(attributes, :visible, Observable(true))
+    strokewidth = get!(attributes, :strokewidth, Observable(0))
+    attributes[:visible] = @lift $visible && $strokewidth > 0
     attributes[:color] = get(attributes, :strokecolor, :black)
-    attributes[:linewidth] = get(attributes, :strokewidth, 0)
+    attributes[:linewidth] = strokewidth
     Makie.lines!(plt, shape; attributes...)
 end
 
